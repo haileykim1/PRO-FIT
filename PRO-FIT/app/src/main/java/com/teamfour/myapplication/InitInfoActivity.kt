8ece3075113165ea065d90e1989cac0a87df4161
haileykim1 <49053341+haileykim1@users.noreply.github.com>
2021-03-30 11:06:32 +0900

    Building Scenes Before MainActivity (SplashActivity.kt, InitInfoActivity.kt)

---
+++ PRO-FIT/app/src/main/java/com/teamfour/myapplication/InitInfoActivity.kt
@@ -0,0 +1,12 @@
+package com.teamfour.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InitInfoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_init_info)
+    }
+}
