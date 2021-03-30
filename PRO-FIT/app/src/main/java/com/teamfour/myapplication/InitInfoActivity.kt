c33668f57f26fa9c315fff830650a13cfa46761c
haileykim1 <49053341+haileykim1@users.noreply.github.com>
2021-03-30 13:53:00 +0900

    Constructing Swiches between Scenes at the beginning(Splash -> Info -> Main)

--- PRO-FIT/app/src/main/java/com/teamfour/myapplication/InitInfoActivity.kt
+++ PRO-FIT/app/src/main/java/com/teamfour/myapplication/InitInfoActivity.kt
@@ -1,12 +1,37 @@
 package com.teamfour.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_init_info.*
 
 class InitInfoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_init_info)
+
+        button.setOnClickListener{
+
+            //이름, 신장 부분 입력했는지 확인.
+            if((nameEdit.text.isEmpty()) || (heightEdit.text.isEmpty())){
+                Toast.makeText(this, "초기 정보를 전부 입력해주세요", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            //정보 Local에 저장
+            //코드부분
+
+            nextActivity()
+
+        }
+
+    }
+
+    private fun nextActivity(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
--- PRO-FIT/app/src/main/java/com/teamfour/myapplication/SplashActivity.kt
+++ PRO-FIT/app/src/main/java/com/teamfour/myapplication/SplashActivity.kt
@@ -1,12 +1,29 @@
 package com.teamfour.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        //GlobalContext.setContext(this)
+
+        //2초동안 기다렸다가 정보 입력 여부에 따라 다음 액티비티로 넘어감
+        Handler().postDelayed({
+            val intent: Intent = if(false) {
+            Intent(this, MainActivity::class.java)
+            }else{
+                Intent(this, InitInfoActivity::class.java)
+            }
+            startActivity(intent)
+            finish()
+        }, 2000)
+
+        //UserInfo.has(UserInfo.NAME_PASSED) && UserInfo.get(UserInfo.NAME_PASSED) == true
     }
 }
