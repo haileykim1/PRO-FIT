45bcd66fe8e8402e375a6299c4a0984722ecfc6d
haileykim1 <49053341+haileykim1@users.noreply.github.com>
2021-04-07 10:20:16 +0900

    Pose tracking - draw_landmarks

--- PoseTracking/PoseTrackingMin.py
+++ PoseTracking/PoseTrackingMin.py
@@ -3,3 +3,28 @@ import mediapipe as mp
 import time
 
 
+video = cv2.VideoCapture("C:/mtest/Boy.mp4")
+
+mpDrawing = mp.solutions.drawing_utils
+mpPose = mp.solutions.pose
+
+#성능에 따라 pose.py보고(Pose()함수 Ctrl + 좌클) parameter 바꾸기
+#클래스는 RGB이미지만 처리가능
+pose = mpPose.Pose()
+
+while(video.isOpened()):
+    ret, img = video.read()
+
+    #RGB영상으로 convert
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = pose.process(imgRGB)
+    #print(results)
+
+    mpDrawing.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
+
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
+
+
+
