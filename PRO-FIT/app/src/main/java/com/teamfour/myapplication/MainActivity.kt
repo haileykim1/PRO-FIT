b375156e41f6404b1120ae0c83d05bac3cc32fc6
haileykim1 <49053341+haileykim1@users.noreply.github.com>
2021-10-13 07:15:53 +0900

    Add Android layout Armature rotation

--- PRO-FIT/app/src/main/java/com/teamfour/myapplication/MainActivity.kt
+++ PRO-FIT/app/src/main/java/com/teamfour/myapplication/MainActivity.kt
@@ -1,12 +1,51 @@
 package com.teamfour.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Init()
     }
+
+    private fun Init(){
+        val intent = Intent(this, VirtualFittingActivity::class.java)
+        startActivity(intent)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.drawer, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.item1 ->{
+                val intent = Intent(this, VirtualFittingActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            R.id.item2 ->{
+                val intent = Intent(this, ProfileActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            R.id.item3 -> {
+                val intent = Intent(this, ClothesActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> super.onContextItemSelected(item)
+        }
+
+    }
+
 }
---
+++ PRO-FIT/app/src/main/java/com/teamfour/myapplication/VirtualFittingActivity.kt
@@ -0,0 +1,11 @@
+package com.teamfour.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VirtualFittingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_virtual_fitting)
+    }
+}
